3aeb2810d46aa8e3490deb5d921b97f9038f9df2
NotYourSyn <60495598+NotYourSyn@users.noreply.github.com>
2020-10-06 21:50:43 -0400

    added some styling

--- react-github-user-card/src/Components/Card.js
+++ react-github-user-card/src/Components/Card.js
@@ -4,9 +4,9 @@ import React from 'react';
 const Card = props => {
 
     return (
-        <div>
-            <img src={props.profile.avatar_url} width='150px' height='150px' alt={`${props.profile.name}'s avatar`}/>
-            <div>
+        <div className='card'>
+            <img src={props.profile.avatar_url} width='550px' height='550px' alt={`${props.profile.name}'s avatar`}/>
+            <div className='card-info'>
                 <p><strong>Username:</strong> {props.profile.login}</p>
                 <p><strong>Followers:</strong> {props.profile.followers}</p>
                 <p><strong>Following:</strong> {props.profile.following}</p>
--- react-github-user-card/src/Components/UserCard.js
+++ react-github-user-card/src/Components/UserCard.js
@@ -3,7 +3,7 @@ import Card from './Card';
 
 const UserCards = (props) => {
     return(
-        <div>
+        <div className='card-container'>
             <Card profile={props.gitCard}/>
         </div>
     )
